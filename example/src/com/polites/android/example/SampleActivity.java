e69b22902c7f5a719b8ea365f9984e32dd6d1794
Jason Polites <jason.polites@gmail.com>
2012-02-18 21:47:13 -0800

    Added separate sample project

---
+++ example/src/com/polites/android/example/SampleActivity.java
@@ -0,0 +1,13 @@
+package com.polites.android.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
