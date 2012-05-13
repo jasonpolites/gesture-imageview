9968c0b27821b05ac5c89bfb1a3ce7673ba8fd14
Jason Polites <jason.polites@gmail.com>
2012-05-12 18:13:42 -0700

    Added dialog to onclick example

--- example/src/com/polites/android/example/StandardImageProgrammaticWithOnClick.java
+++ example/src/com/polites/android/example/StandardImageProgrammaticWithOnClick.java
@@ -1,6 +1,7 @@
 package com.polites.android.example;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,7 +14,16 @@ public class StandardImageProgrammaticWithOnClick extends StandardImageProgramma
     	view.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				new AlertDialog.Builder(v.getContext()).setMessage("test").create().show();
+				new AlertDialog.Builder(v.getContext())
+				.setMessage("Clicked!")
+				.setTitle("onClickTest")
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				})
+				.create().show();
 			}
 		});
     }
